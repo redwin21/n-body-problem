518cf65ada43282669004ef0c25e496325c86061
EC2 Default User <ec2-user@ip-172-31-10-36.us-west-2.compute.internal>
2020-06-04 00:02:18 +0000

    create table generator function and test

--- simulator/table_data.py
+++ simulator/table_data.py
@@ -1,21 +1,33 @@
 import numpy as np
 import pandas as pd
 from pymongo import MongoClient
+import multiprocessing as mp
 
 
-def tabularize(collection, steps, size=100, n_bodies=3, com=False):
+def tabularize(collection_name, steps, size=100, n_bodies=3, com=False, path=None):
+    
+    client = MongoClient('localhost', 27017)
+    db = client['n-body']
+    collection = db[collection_name]
     
     col = ['sim_id']
     for n in range(1,n_bodies+1):
         col.append(f'm_{n}')
     
-    initial = ['rx_','ry_','rz_','vx_','vy_','vz_']
+    r_initial = ['rx_','ry_','rz_']
+    v_initial = ['vx_','vy_','vz_']
     for step in ([0]+steps):
-        for i in initial:
-            col.append(i + str(step))
+        for n in range(1,n_bodies+1):
+            for i in r_initial:
+                col.append(i + str(n) + '_' + str(step))
+        for n in range(1,n_bodies+1):
+            for i in v_initial:
+                col.append(i + str(n) + '_' + str(step))
     
     counter = 0
-    df = pd.DataFrame(columns=cols)
+    df = pd.DataFrame(columns=col)
+    name = collection.name
+    
     for idx, entry in enumerate(collection.find()):
     
         if com:
@@ -26,22 +38,28 @@ def tabularize(collection, steps, size=100, n_bodies=3, com=False):
         for i in range(size):
             
             df_entry = [entry['id']]
+            for n in range(n_bodies):
+                df_entry.append(entry['mass'][n][0])
             
             rand_idx = np.random.choice(len(sim) - steps[-1] - 1)
             
             for step in ([0]+steps):
                 df_entry += sim[step + rand_idx]
             
-            df[counter] = df_entry
+            print(f'Data point {counter:6.0f} of table {name}')
+            
+            df.loc[counter] = df_entry
             counter += 1
+            
+    if not path is None:
+        df.to_csv(path, index=False)
     
     return df
 
 
+
+
 def main():
-    
-    client = MongoClient('localhost', 27017)
-    db = client['n-body']
     
     collections = ['samples_2_bodies_3_dim_1_m', 
                   'samples_2_bodies_3_dim_0_m',
@@ -49,16 +67,23 @@ def main():
                   'samples_3_bodies_3_dim_0_m']
     
     steps = [10, 100, 1000]
+    processes = []
             
     for collection in collections:
         
-        df = tabularize(db[collection], steps)
-        path = f'../data/{collection}.csv'
-        df.to_csv(path)
+        n_bodies = int(collection.split('_')[1])
+        path = f'./data/{collection}.csv'
+        
+        p = mp.Process(target=tabularize, args=(collection, steps, 100, n_bodies, False, path))
+        processes.append(p)
+        p.start()
+    
+    for process in processes:
+        process.join()
         
     pass
 
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     main()
         
