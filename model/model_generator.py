e1444c43d8bde108f140cd81ad2d31d481ee6dab
Eddie Ressegue <redwin21@gmail.com>
2020-06-05 17:35:36 -0700

    update docstring

--- model/model_generator.py
+++ model/model_generator.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import pickle
 import multiprocessing as mp
-from transform import get_X_y, transform_X_y, inverse_transform_X_y, normalize
+from model.transform import get_X_y, transform_X_y, inverse_transform_X_y, normalize
 
 from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
 from sklearn.preprocessing import StandardScaler
@@ -13,8 +13,21 @@ from sklearn.multioutput import MultiOutputRegressor
 
 
 class GSModel():
+    """
+    A class to build a gradient boosting model with the added capability of doing
+    predictions on non-transformed data.
+    """
     
     def __init__(self, n_bodies, steps, parameters, use_mass=False):
+        """
+        Initialize model.
+        
+        Inputs:
+            n_bodies - how many bodies are in the data
+            steps - time horizon of target data choice. e.g. 10, 100, or 1000
+            parameters - parameter dictionary for the grid search
+            use_mass - whether or not to include mass in the output. if not, masses of 1 are assumed
+        """
         
         self.model = None
         self.parameters = parameters
@@ -24,6 +37,9 @@ class GSModel():
         
         
     def fit(self, X, y):
+        """
+        Fit the data using the sci-kit learn standards.
+        """
 
         model = MultiOutputRegressor(GradientBoostingRegressor(), n_jobs=-1)
         self.model = GridSearchCV(estimator=model, 
@@ -39,6 +55,10 @@ class GSModel():
     
     
     def predict_(self, X, transform=True):
+        """
+        Make predictions of the data, while performing a transformation first and an
+        inverse transformation after.
+        """
         
         if transform:
             X = transform_X_y(X, self.n_bodies, self.use_mass)
@@ -52,18 +72,41 @@ class GSModel():
         
 
     def model_error(self, X, y):
+        """
+        Calculate model error using root-mean-squared-error.
+        """
         
         return np.sqrt(mean_squared_error(y, self.predict_(X, transform=False)))
 
 
 def get_model_data(path, steps, use_mass=False):
+    """
+    Load, transform, and split the data for use in model training and testing.
+    
+    Inputs:
+        path - filepath of data
+        steps - time horizon of target data choice. e.g. 10, 100, or 1000
+        use_mass - whether or not to include mass in the output. if not, masses of 1 are assumed
+        
+    Returns:
+        X_train, X_test, y_train, y_test
+    """
     
     df = pd.read_csv(path)
     
     X, y = get_X_y(df, steps, use_mass, transform=True)
     return train_test_split(X, y)
 
+
 def build_model(path, step, gs_params):
+    """
+    Builds the model, fits it, and pickles it.
+    
+    Inputs:
+        path - filepath of data
+        step - time horizon of target data choice. e.g. 10, 100, or 1000
+        gs_params - parameter dictionary for the grid search
+    """
     
     save_path = path.split('/')[-1].split('.')[0] + f'_steps_{step}.pkl'
     n_bodies = int(path.split('/')[-1].split('_')[1])
@@ -79,6 +122,10 @@ def build_model(path, step, gs_params):
     print('Fit model: ', save_path, '    model error: ', model.model_error(X_test, y_test))
 
 def main():
+    """
+    Build and save the models of interest with various time steps for 2 and 3 bodies.
+    Runs multiprocessed.
+    """
     
     gs_params = {
                 'estimator__n_estimators': [100, 500, 1000, 2000],
